c246aa6eb9b5ce7bb052c5c73b68b8b393a8967e
mclam <mclam@student.42wolfsburg.de>
2022-09-03 20:52:50 +0200

    ex02 done

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mclam <mclam@student.21-school.ru>         +#+  +:+       +#+        */
+/*   By: mclam <mclam@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/02 22:29:14 by mclam             #+#    #+#             */
-/*   Updated: 2022/09/02 22:29:14 by mclam            ###   ########.fr       */
+/*   Created: 2022/09/03 17:05:32 by mclam             #+#    #+#             */
+/*   Updated: 2022/09/03 17:05:32 by mclam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,56 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# define OUTPUT_LIMIT	4
 
-template<class T> class Array
+template<class Template> class Array
 {
 protected:
-	T	*array;
-	int	size;
+	Template		*_array;
+	unsigned int	_size;
 public:
+	// Canonical
+					Array( void ) : _array( NULL ), _size( 0 ) {}
+					Array( const Array<Template> &array) : _array( NULL ), _size( 0 ) { *this = array; }
+	virtual			~Array() { delete [] _array; }
+	Array<Template>	&operator=( const Array<Template> &array) {
+		if (this != &array) {
+			if(_size > 0) delete [] _array;
+			_size = array._size;
+			_array = new Template[_size];
+			for (unsigned int i = 0; i < array._size; i++) _array[i] = array._array[i];
+		}
+		return *this;
+	}
 	
+	// Other member functions
+					Array( unsigned int size) : _array( new Template[size] ), _size( size ) {}
+	unsigned int	size( void ) const { return _size; }
+	Template		&operator[]( const long i ) const {
+		if (i < 0 || static_cast<unsigned int>(i) >= _size) throw IndexOutOfBoundsException();
+		return _array[i];
+	}
+
+	// Index Out Of Bounds Exception
+	class IndexOutOfBoundsException  : public std::exception {
+	public:
+		virtual const char *what() const throw() { return "Index out of bounds"; }
+	};
 };
+
+template<typename Template> std::ostream	&operator<<(std::ostream &out, const Array<Template> &array) {
+	if (array.size() > 0) {
+		unsigned int	n = array.size() - 1;
+			
+		if (n > OUTPUT_LIMIT) {
+			for (unsigned int i = 0; i < OUTPUT_LIMIT; i++) out << array[i] << ", ";
+			out	<< "..., ";
+		}
+		else
+			for (unsigned int i = 0; i < n; i++) out << array[i] << ", ";
+		out	<< array[n];
+	}
+	return out;
+}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,16 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mclam <mclam@student.21-school.ru>         +#+  +:+       +#+        */
+/*   By: mclam <mclam@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/02 21:57:29 by mclam             #+#    #+#             */
-/*   Updated: 2022/09/02 21:57:29 by mclam            ###   ########.fr       */
+/*   Created: 2022/09/03 17:05:53 by mclam             #+#    #+#             */
+/*   Updated: 2022/09/03 17:05:53 by mclam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
 
-int main() {
-	std::cout << "Hello, World!\n";
+#define MAX_VAL 5
+
+int main(int, char**) {
+	Array<int>	numbers(MAX_VAL);
+	int			*mirror = new int[MAX_VAL];
+	
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < MAX_VAL; i++) {
+		const int	value = std::rand();
+		
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	std::cout	<< "numbers[" << numbers.size() << "] = {"
+				<< numbers << "}" << std::endl;
+
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+
+	for (int i = 0; i < MAX_VAL; i++) {
+		if (mirror[i] != numbers[i]) {
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try {
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try {
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	for (int i = 0; i < MAX_VAL; i++) {
+		numbers[i] = rand();
+	}
+	std::cout	<< "numbers[" << numbers.size() << "] = {"
+				<< numbers << "}" << std::endl;
+	delete [] mirror;//
 	return 0;
 }
